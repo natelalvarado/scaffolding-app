8357c2507ffbe26220f24fe0eb497c261e199c51
Nate <natelalvarado@gmail.com>
2021-10-13 18:44:48 -0400

    Creates Scaffolding App and adds formatting changes

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  attr_accessor :content, :name, :title
+ 
+  validates :name,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+end
+
